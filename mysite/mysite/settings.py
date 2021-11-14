6bee88b486e9b96f992a4daf67b92a71b15f44de
Jae Won Choi <chjwon10044@gmail.com>
2021-11-14 14:02:47 -0600

    roll back DEBUG = True

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+4$s3p+e9$(y!(z=q^m+d-x2gs5w*)!jf46c+absjg@f5f&*-2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
